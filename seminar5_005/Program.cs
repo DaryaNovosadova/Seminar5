e21a9f3eb78bebd2450236e6a47ea8c2258b7940
Darya <darya.novosadova@gmail.com>
2022-11-10 16:24:20 +0700

    seminar5_005

---
+++ seminar5_005/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 37: Найдите произведение пар чисел в 
+одномерном массиве. Парой считаем первый и 
+последний элемент, второй и предпоследний и т.д. 
+Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21*/
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(6, 0, 9);
+Console.WriteLine();
+
+int[] arr = new int[array.Length / 2];
+
+int size = array.Length - 1;
+int ind = 0;
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    arr[ind] = array[i] * array[size];
+    Console.Write($"{arr[ind]} ");
+    size--;
+    ind++;
+}
